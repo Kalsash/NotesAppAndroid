c3526ceb841b66856d363ce89131157afb1f766b
Александр Калинин <kalsash@yandex.ru>
2022-08-11 18:47:30 +0300

    NotesDB: класс-репозиторий для БД

---
+++ Notes/Notes/Models/NotesDB.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+using Notes.Models;
+using System.Threading.Tasks;
+
+namespace Notes.Models
+{
+    internal class NotesDB
+    {
+        readonly SQLiteAsyncConnection db;
+        public NotesDB(string connectionString)
+        {
+            db = new SQLiteAsyncConnection(connectionString);
+            db.CreateTableAsync<Note>().Wait();
+        }
+        public Task<List<Note>> GetNotesAsync()
+        {
+            return db.Table<Note>().ToListAsync();
+        }
+        public Task<Note> GetNoteAsync(int id)
+        {
+            return db.Table<Note>().Where(i => i.Id == id).FirstOrDefaultAsync();
+        }
+        public Task<int> SaveNoteAsync(Note note)
+        {
+            if (note.Id != 0)
+            {
+                return db.UpdateAsync(note);
+            }
+            return db.InsertAsync(note);
+        }
+        public Task<int> DeleteNoteAsync(Note note)
+        {
+
+            return db.DeleteAsync(note);
+        }
+    }
+}
